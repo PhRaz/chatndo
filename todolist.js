60899942a12dcae2835511bb0f11e01e54caad43
PhRaz <philippe.razavet@gmail.com>
2018-01-12 22:40:23 +0100

    correction et finition

--- todolist.js
+++ todolist.js
@@ -1,29 +1,37 @@
+/*
+ *  gestion de todo list
+ */
 var express = require('express');
-
 var cookieSession = require('cookie-session'); // Charge le middleware de sessions
 var bodyParser = require('body-parser'); // Charge le middleware de gestion des paramètres
-var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 var app = express();
 
 /*
  * On utilise les cookies sessions et le décodage des body urlEncoded
  */
-app
-    .use(cookieSession({
-        name: 'session',
-        secret: 'todotopsecret',
-        maxAge: 24 * 60 * 60 * 1000
-    }))
-    .use(urlencodedParser);
+app.use(cookieSession({
+    name: 'session',
+    secret: 'todotopsecret',
+    maxAge: 24 * 60 * 60 * 1000
+}));
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 /*
- * liste des tâches
+ * création de la liste dans la session si elle n'existe pas
  */
-app.get('/todo', function (req, res) {
-    if (typeof req.session.todoListe !== 'undefined') {
+app.use(function (req, res, next) {
+    if (typeof req.session.todoListe === 'undefined') {
         req.session.todoListe = [];
     }
+    next();
+});
+
+/*
+ * lister les tâches
+ */
+app.get('/todo', function (req, res) {
     res.render('./todolist.ejs', {todo_liste: req.session.todoListe});
 });
 
@@ -31,18 +39,24 @@ app.get('/todo', function (req, res) {
  * ajouter une tâche
  */
 app.post('/todo/ajouter', function (req, res) {
-    req.session.todoListe.push(req.body.trucAFaire);
-    res.render('./todolist.ejs', {todo_liste: req.session.todoListe});
+    if (req.body.trucAFaire !== '') {
+        req.session.todoListe.push(req.body.trucAFaire);
+    }
+    res.redirect('/todo');
 });
 
 /*
  * supprimer une tâche
  */
 app.get('/todo/supprimer/:id', function (req, res) {
-    req.session.todoListe = req.session.todoListe.filter(function (value, index) {
-        return index !== parseInt(this[0]);
-    }, req.params.id);
-    res.render('./todolist.ejs', {todo_liste: req.session.todoListe});
+    if (req.params.id !== '') {
+        req.session.todoListe.splice(req.params.id, 1);
+    }
+    res.redirect('/todo');
+});
+
+app.use(function (req, res) {
+    res.redirect('/todo');
 });
 
 app.listen(8080);
