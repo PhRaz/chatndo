699e63ac7c6e9d3ce688fd66e18e88d6507f25c5
Philippe Razavet <philippe.razavet-prestaire@laposte.fr>
2018-02-06 12:03:01 +0100

    ajout suppression des items

--- todolist.js
+++ todolist.js
@@ -1,27 +1,21 @@
 /*
- *  gestion de todo list
- *
- * utilisation d'un id pour les items de la liste
+ * gestion de todo list multi utilisateur
  */
 
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
-var bodyParser = require('body-parser'); // Charge le middleware de gestion des paramètres
-var path = require('path');
 var ent = require('ent');
 
-
+/*
+ * Gestion de la liste dans une variable globale du serveur.
+ * Chaque item de liste est associé à un identifiant.
+ */
 var todoListe = [];
 var todoId = 0;
 
 /*
- * décodage des bodies urlEncoded
- */
-app.use(bodyParser.urlencoded({extended: false}));
-
-/*
- * le get (chargement de la page) renvoie la page avec la liste courante
+ * le get renvoie la page avec la liste courante
  */
 app.get('/todo', function (req, res, next) {
     res.render('./todolist.ejs', {todo_liste: todoListe});
@@ -31,29 +25,23 @@ app.get('/todo', function (req, res, next) {
  * connexion d'un nouveau client
  */
 io.on('connection', function (socket) {
-    console.log("connextion d'un utilisateur");
-
-    socket.emit('serveur_demande_pseudo');
-
-    socket.on('client_deconnexion', function () {
-        console.log('un utilisateur est déconnecté');
-    });
-
-    socket.on('client_renvoie_pseudo', function (msg) {
-        console.log("connexion de " + ent.encode(msg));
-        socket.emit('serveur_emet_todo_list', todoListe);
-    });
+    console.log("connexion d'un utilisateur");
 
     socket.on('client_ajoute_todo', function (msg) {
-        console.log('ajout todo ' + msg);
-        todoListe.push({ todoId: todoId, pseudo: msg.pseudo, todoItem: ent.encode(msg.message)});
+        console.log('ajout todo ' + todoId + ' : ' + msg);
+        todoListe.push({ todoId: todoId, todoItem: ent.encode(msg)});
         todoId++;
         socket.broadcast.emit('serveur_emet_todo_list', todoListe);
         socket.emit('serveur_emet_todo_list', todoListe);
     });
 
     socket.on('client_supprime_todo', function(msg) {
-
+        todoListe = todoListe.filter(function(e) {
+            return (e.todoId !== parseInt(msg));
+        });
+        socket.broadcast.emit('serveur_emet_todo_list', todoListe);
+        socket.emit('serveur_emet_todo_list', todoListe);
+        console.log("suppression todo " + msg);
     });
 });
 
@@ -68,5 +56,5 @@ app.use(function (req, res, next) {
  * démarrage du serveur
  */
 server.listen(8080, function () {
-    console.log('listening on *:8080');
+    console.log('le serveur écoute sur *:8080');
 });
