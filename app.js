db857e76568cf68350235263d86007057a2254ea
Philippe Razavet <philippe.razavet-prestaire@laposte.fr>
2018-02-16 16:44:55 +0100

    manage sessions

--- app.js
+++ app.js
@@ -5,7 +5,9 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
+var session = require('express-session');
 var ent = require('ent');
+var bodyParser = require('body-parser');
 
 /*
  * Gestion de la liste dans une variable globale du serveur.
@@ -15,58 +17,65 @@ var todoListe = [];
 var todoId = 0;
 
 /*
- * le get renvoie la page de l'application
+ * configuration
  */
-app.get('/todo', function (req, res, next) {
-    res.render('./todolist.ejs');
+app.use(session({
+    secret: 'top secret',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(function (req, res, next) {
+    if (!req.session.pseudo) {
+        req.session.pseudo = '';
+    }
+    next();
 });
 
 /*
- * connexion d'un nouveau client
+ * login page request
  */
-io.on('connection', function (socket) {
-    console.log("connexion d'un utilisateur");
-
-    socket.emit('demande_pseudo');
-
-    socket.on('disconnect', function () {
-        console.log('user disconnected');
-    });
-
-    socket.on('pseudo', function (msg) {
-        message = ent.encode(msg)  + ' a rejoint la conversation';
-        console.log(message);
-        socket.broadcast.emit('message', message);
-        socket.emit('message', message);
-
-        socket.emit('serveur_emet_todo_list', todoListe);
-    });
-
-    socket.on('message', function (msg) {
+app.get('/login', function (req, res, next) {
+    console.log("login");
+    if (req.session.pseudo !== '') {
         /*
-         * encodage des entitées HTML pour ne pas injecter de HTML dans la page
+         * already logged in
          */
-        message = '<span class="pseudo">' + ent.encode(msg.pseudo) + '</span> ' + ent.encode(msg.message);
-        console.log(message);
-        socket.broadcast.emit('message', message);
-        socket.emit('message', message);
-    });
+        res.redirect('/todo');
+    } else {
+        res.render('./login.ejs');
+    }
+});
 
-    socket.on('client_ajoute_todo', function (msg) {
-        console.log('ajout todo ' + todoId + ' : ' + msg);
-        todoListe.push({ todoId: todoId, todoItem: ent.encode(msg)});
-        todoId++;
-        socket.broadcast.emit('serveur_emet_todo_list', todoListe);
-        socket.emit('serveur_emet_todo_list', todoListe);
-    });
+/*
+ * login page form submit
+ */
+app.post('/login', function(req, res, next) {
+    if (req.body) {
+        if (req.body.pseudo) {
+            /*
+             * submit login
+             */
+            req.session.pseudo = req.body.pseudo;
+            res.redirect('/todo');
+        }
+    }
+    res.render('./login.ejs');
+});
 
-    socket.on('client_supprime_todo', function(msg) {
-        todoListe = todoListe.filter(function(e) {
-            return (e.todoId !== parseInt(msg));
-        });
-        socket.broadcast.emit('serveur_emet_todo_list', todoListe);
-        socket.emit('serveur_emet_todo_list', todoListe);
-        console.log("suppression todo " + msg);
+/*
+ * logout request
+ */
+app.get('/logout', function (req, res, next) {
+    req.session.destroy(function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/todo');
+        }
     });
 });
 
@@ -75,6 +84,64 @@ io.on('connection', function (socket) {
  */
 app.use(function (req, res, next) {
     res.redirect('/todo');
+});
+
+/*
+ * renvoie la page de l'application
+ */
+app.get('/todo', function (req, res, next) {
+    if (req.session.pseudo !== '') {
+        res.render('./todolist.ejs', {pseudo: req.session.pseudo});
+    } else {
+        res.redirect('/login');
+    }
+});
+
+/*
+ * connexion d'un nouveau client
+ */
+io.on('connection', function (socket) {
+
+    /*
+    message = pseudo + ' a rejoint la conversation';
+    console.log(message);
+
+    socket.broadcast.emit('message', message);
+    socket.emit('message', message);
+
+    socket.on('disconnect', function () {
+        console.log('[recharge la page ?] déconnexion');
+    });
+    */
+
+    socket.emit('serveur_emet_todo_list', todoListe);
+
+    socket.on('message', function (msg) {
+        /*
+         * encodage des entitées HTML pour ne pas injecter de HTML dans la page
+         */
+        message = '<span class="pseudo">' + ent.encode(msg.pseudo) + '</span> ' + ent.encode(msg.message);
+        console.log(message + " " + socket.request.pseudo);
+        socket.broadcast.emit('message', message);
+        socket.emit('message', message);
+    });
+
+    socket.on('client_ajoute_todo', function (msg) {
+        console.log('ajout todo ' + todoId + ' : ' + msg);
+        todoListe.push({todoId: todoId, todoItem: ent.encode(msg)});
+        todoId++;
+        socket.broadcast.emit('serveur_emet_todo_list', todoListe);
+        socket.emit('serveur_emet_todo_list', todoListe);
+    });
+
+    socket.on('client_supprime_todo', function (msg) {
+        todoListe = todoListe.filter(function (e) {
+            return (e.todoId !== parseInt(msg));
+        });
+        socket.broadcast.emit('serveur_emet_todo_list', todoListe);
+        socket.emit('serveur_emet_todo_list', todoListe);
+        console.log("suppression todo " + msg);
+    });
 });
 
 /*
